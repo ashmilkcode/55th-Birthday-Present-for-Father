475d831f1b7fd02ab30d1ef905c9723b74d2129e
Lakshmi <84883977+ashmilkcode@users.noreply.github.com>
2023-06-03 23:23:36 -0400

    Add files via upload

---
+++ tensorflow.js
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <title>Daddy's 55th birthday present</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.js"></script>
+    <script src="https://unpkg.com/ml5@0.4.3/dist/ml5.min.js"></script>
+  
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Doppio+One&family=Koulen&family=Mali:wght@200&family=Mukta:wght@300&family=Oleo+Script+Swash+Caps&family=Open+Sans&family=Patua+One&family=Sacramento&family=Single+Day&family=Tilt+Warp&display=swap" rel="stylesheet">
+  </head>
+  <body background: "https://motionarray.imgix.net/preview-171620-PmuoFmnd17-high_0002.jpg?w=660&q=60&fit=max&auto=format">
+  <center>
+   <h1>Happy Birthday Daddy!</h1>
+   <h2>55th ~ June 6th 2023</h2>
+   <h3>Here's a cringey AI model to celebrate your special double number birthday:</h3>
+  </center>
+  </body>
+  <center></center>
